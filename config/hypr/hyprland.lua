7b9b78a684869418ec07bae581b70b75dae7015a
hacky <jon.nguyen7@protonmail.com>
2026-06-01 10:35:08 -0500

    work monitor

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -8,14 +8,12 @@ require("modules.workspace_rules")
 require("modules.look")
 require("modules.configs")
 
-hl.monitor({ output = "HDMI-A-2", mode = "2560x2880@60", position = "0x0", scale = "1.25" })
-hl.monitor({ output = "HDMI-A-1", mode = "2560x1440@120", position = "2048x720", scale = "1" })
+hl.monitor({ output = "DP-5", mode = "3440x1440@120", position = "0x0", scale = "1" })
 hl.monitor({ output = "eDP-2", mode = "2560x1600@165", position = "0x0", scale = "1.25" })
 
 hl.on("hyprland.start", function()
 	hl.exec_cmd(TERMINAL)
 	hl.exec_cmd("/home/hacky/hacky_nix/scripts/startup.sh")
-	-- hl.exec_cmd("startup.sh")
 end)
 
 hl.env("XCURSOR_SIZE", "22")
